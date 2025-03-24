3e5254b9f8b2473ec60bc828b560569747a2e6b5
djblanc360 <djblanc360@gmail.com>
2025-03-24 13:14:16 -0700

    updated mock data and extrapolated file row

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,13 +9,6 @@ import * as schema from "./db/schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-// const globalForDb = globalThis as unknown as {
-//   client: Client | undefined;
-// };
-
-// export const client =
-//   globalForDb.client ?? createClient({ url: process.env.DATABASE_URL });
-// if (process.env.NODE_ENV !== "production") globalForDb.client = client;
 
 const globalForDb = globalThis as unknown as {
     poolConnection: Pool | undefined;
