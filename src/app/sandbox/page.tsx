38b33939848aaf75c993fd31929bb35be353a01f
djblanc360 <djblanc360@gmail.com>
2025-03-24 16:13:21 -0700

    created schema and tested with sandbox ui

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,56 @@
+import { db } from "../../server";
+import { mockFolders, mockFiles } from "../../lib/mock-data";
+import { folders, files } from "../../server/db/schema";
+
+export default function SandboxPage() {
+    return (
+        <div className="flex flex-col gap-4">
+            <form action={async () => {
+                "use server"
+                console.log("Seeding...")
+                /*
+                    const folderInsert = await db.insert(folders).values(
+                        mockFolders.map((folder, index) => ({
+                            id: index + 1,
+                            name: folder.name,
+                            parent: index !== 0 ? index : null,
+                        })
+                    ))
+
+                    const fileInsert = await db.insert(files).values(
+                        mockFiles.map((file, index) => ({
+                            id: index + 1,
+                            name: file.name,
+                            size: parseInt(file.size),
+                            url: file.url,
+                            createdAt: new Date(),
+                            updatedAt: new Date(),
+                            parent: (index% 3) + 1, // random parent
+                        }))
+                    )
+                    console.log("Seeded", folderInsert.length, "folders and", fileInsert.length, "files: ", folderInsert, fileInsert)
+                    */
+                // Clear existing data first
+                // await db.delete(files)
+                // await db.delete(folders)
+                
+                await db.insert(folders).values(mockFolders.map((folder, index) => ({
+                    id: index + 1,
+                    name: folder.name,
+                    parent: index !== 0 ? index : null,
+                })))
+                await db.insert(files).values(mockFiles.map((file, index) => ({
+                    id: index + 1,
+                    name: file.name,
+                    size: parseInt(file.size),
+                    url: file.url,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    parent: (index% 3) + 1, // random parent
+                })))
+            }}>
+                <button type="submit">Seed</button>
+            </form>
+        </div>
+    )
+}
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,11 +1,33 @@
-import { bigint, int, text, timestamp, singlestoreTable, singlestoreTableCreator } from "drizzle-orm/singlestore-core";
+import { bigint, int, text, timestamp, singlestoreTableCreator, index } from "drizzle-orm/singlestore-core";
 
-// export const createTable = singlestoreTableCreator(
-//   (name) => `cloud-storage_${name}`,
-// );
+export const createTable = singlestoreTableCreator(
+  (name) => `cloud-storage_${name}`,
+);
 
-export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
+// export const users = createTable("users", {
+//   id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
+//   name: text("name"),
+//   createdAt: timestamp("created_at").notNull().defaultNow(),
+// });
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
-});
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+  
+}, (t) => {
+  return [index("parent_index").on(t.parent)]
+})
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+  
+}, (t) => {
+  return [index("parent_index").on(t.parent)]
+})
